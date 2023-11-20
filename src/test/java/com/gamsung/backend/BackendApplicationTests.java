f82da516308b33c2ea87d84858f5dd00dcfc49de
rightpair <tkddn204@gmail.com>
2023-11-20 11:27:42 +0900

    Feat: 초기 설정
    Motivation:
    - 프로젝트 준비를 위해 초기 설정을 하려 합니다.
    
    Modification:
    - 초기설정

---
+++ src/test/java/com/gamsung/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gamsung.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
