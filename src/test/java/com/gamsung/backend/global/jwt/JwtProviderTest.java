7fd58be3da26e83d28289db03fe93beb5d33a534
rightpair <tkddn204@gmail.com>
2023-11-22 15:51:21 +0900

    Test: JwtProvider 테스트 코드 작성
    Motivation:
    - JwtProvider가 올바르게 작동하는지를 확인하기 위해 테스트 코드를 작성하려 합니다.
    
    Modification:
    - `JwtProviderTest.java` 추가

---
+++ src/test/java/com/gamsung/backend/global/jwt/JwtProviderTest.java
@@ -0,0 +1,96 @@
+package com.gamsung.backend.global.jwt;
+
+import com.gamsung.backend.global.jwt.dto.JwtPayload;
+import com.gamsung.backend.global.jwt.exception.JwtInvalidTokenException;
+import com.gamsung.backend.global.jwt.exception.JwtTokenExpiredException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.Date;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
+class JwtProviderTest {
+
+    @Autowired
+    private JwtProvider jwtProvider;
+
+    @Value("${service.jwt.access-expiration}")
+    private long accessTokenExpiration;
+
+    @DisplayName("Jwt 토큰 발급")
+    @Nested
+    class CreateJwtToken {
+
+        private static final String TEST_EMAIL = "test@test.com";
+
+        @DisplayName("createToken 호출에 성공한다.")
+        @Test
+        void successToCreateJwtToken() {
+            // given
+            Date issuedAt = new Date(System.currentTimeMillis());
+            JwtPayload jwtPayload = JwtPayload.builder()
+                    .email(TEST_EMAIL)
+                    .issuedAt(issuedAt)
+                    .build();
+
+            // when
+            String jwtToken = jwtProvider.createToken(jwtPayload, accessTokenExpiration);
+
+            // then
+            JwtPayload verifiedJwtPayload = jwtProvider.verifyToken(jwtToken);
+            Assertions.assertEquals(TEST_EMAIL, verifiedJwtPayload.getEmail());
+            Assertions.assertEquals(issuedAt.getTime() / 1000, verifiedJwtPayload.getIssuedAt().getTime() / 1000);
+        }
+    }
+
+    @DisplayName("Jwt 토큰 검증 실패")
+    @Nested
+    class VerifyJwtToken {
+
+        private static final String WRONG_SECRET_KEY = "Zjk1MDQzY2RhNmE5MTc1ZjA0ZDQwYjhjNTQ1YWQyMjZiZTEwOTQ2N2ExNDk1NWQ1Y2Q0NGI5ZmQyNzMyZjgyNg==";
+        private static final String TEST_EMAIL = "test@test.com";
+
+        @DisplayName("만료된 토큰을 검증할 때 JwtTokenExpiredException 발생")
+        @Test
+        void failToVerifyJwtTokenWhenTokenExpired() {
+            // given
+            Date issuedAt = new Date(System.currentTimeMillis());
+            long shortExpirationTime = 1;
+            JwtPayload jwtPayload = JwtPayload.builder()
+                    .email(TEST_EMAIL)
+                    .issuedAt(issuedAt)
+                    .build();
+
+            // when
+            String jwtToken = jwtProvider.createToken(jwtPayload, shortExpirationTime);
+
+            // then
+            Assertions.assertThrows(JwtTokenExpiredException.class, () -> jwtProvider.verifyToken(jwtToken));
+        }
+
+        @DisplayName("다른 Secret Key로 검증할 때 JwtInvalidTokenException 발생")
+        @Test
+        void failToVerifyJwtTokenWhenWrongSecretKey() {
+            // given
+            Date issuedAt = new Date(System.currentTimeMillis());
+            JwtPayload jwtPayload = JwtPayload.builder()
+                    .email(TEST_EMAIL)
+                    .issuedAt(issuedAt)
+                    .build();
+            JwtProvider otherJwtProvider = new JwtProvider(WRONG_SECRET_KEY);
+
+            // when
+            String wrongToken = otherJwtProvider.createToken(jwtPayload, accessTokenExpiration);
+
+            // then
+            Assertions.assertThrows(JwtInvalidTokenException.class, () -> jwtProvider.verifyToken(wrongToken));
+        }
+    }
+}
