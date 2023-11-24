d56ddbcf7b3f72a14fbed3883a1044889c80f3f6
rightpair <tkddn204@gmail.com>
2023-11-24 15:03:37 +0900

    Feat: RedisTemplate 빈 추가
    Motivation:
    - RedisTemplate를 사용하기 위해 레디스 설정에서 빈을 추가하려 합니다.
    
    Modification:
    - 레디스 설정에서 RedisTemplate 빈 추가
    
    Signed-off-by: rightpair <tkddn204@gmail.com>

--- src/main/java/com/gamsung/backend/global/config/RedisConfig.java
+++ src/main/java/com/gamsung/backend/global/config/RedisConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
@@ -20,4 +22,11 @@ public class RedisConfig {
         return new LettuceConnectionFactory(redisHost, redisPort);
     }
 
+    @Bean
+    RedisTemplate<String, String> redisTemplate() {
+        RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory());
+        redisTemplate.setDefaultSerializer(new StringRedisSerializer());
+        return redisTemplate;
+    }
 }
