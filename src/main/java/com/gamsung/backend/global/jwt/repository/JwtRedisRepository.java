63af09a6b58515fe0617d9ff4f9294700bef6be5
rightpair <tkddn204@gmail.com>
2023-11-24 15:53:33 +0900

    Feat: RedisTemplate를 사용한 RedisRepository 관련 추가
    Motivation:
    - 레디스 사용을 위한 레포지토리 인터페이스를 추가하려 합니다.
    - RedisTemplate를 사용하여 인터페이스를 통한 클래스를 구현하려 합니다.
    
    Modification:
    - `RedisRepository.java` 추가
    - `JwtRedisRepository.java` 추가
    
    Signed-off-by: rightpair <tkddn204@gmail.com>

--- src/main/java/com/gamsung/backend/global/jwt/repository/JwtRedisRepository.java
+++ src/main/java/com/gamsung/backend/global/jwt/repository/JwtRedisRepository.java
@@ -1,10 +1,38 @@
 package com.gamsung.backend.global.jwt.repository;
 
 import com.gamsung.backend.global.jwt.entity.JwtRedisEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public interface JwtRedisRepository extends JpaRepository<JwtRedisEntity, String> {
+import java.util.Optional;
+import java.util.concurrent.TimeUnit;
 
+@RequiredArgsConstructor
+@Repository
+public class JwtRedisRepository implements RedisRepository<JwtRedisEntity> {
+
+    private final RedisTemplate<String, String> redisTemplate;
+
+    @Override
+    public void save(JwtRedisEntity entity) {
+        redisTemplate.opsForValue()
+                .set(entity.getMemberEmail(), entity.getRefreshToken(),
+                        entity.getExpiration(), TimeUnit.MILLISECONDS);
+    }
+
+    @Override
+    public Optional<String> findByKey(String key) {
+        return Optional.ofNullable(redisTemplate.opsForValue().get(key));
+    }
+
+    @Override
+    public Long getExpire(String key) {
+        return redisTemplate.getExpire(key);
+    }
+
+    @Override
+    public Boolean deleteByKey(String key) {
+        return redisTemplate.delete(key);
+    }
 }
