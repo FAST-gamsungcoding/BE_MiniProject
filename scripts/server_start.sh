ebb0d62e5e1a8c64fbb4167f241500dec8aeceb0
rightpair <tkddn204@gmail.com>
2023-11-25 16:01:49 +0900

    Fix: codedeploy 시 스크립트를 실행하도록 수정
    Motivation:
    - codedeploy의 오류를 수정할 겸, 서버에서 사용할 스크립트를 저장하려 합니다.
    
    Modification:
    - appspec.yml 수정
    - 서버에서 사용할 스크립트들 추가
    
    Signed-off-by: rightpair <tkddn204@gmail.com>

---
+++ scripts/server_start.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+JAR=backend-0.0.1-SNAPSHOT.jar
+LOG=/dev/null
+#LOG=gamsung-console.log
+export spring_profiles_active=prod
+
+./home/ubuntu/server/server_stop.sh
+
+cd ~/server
+nohup java -jar $JAR > $LOG 2>&1 &
