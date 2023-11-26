ea7c77ff4ebb2df0c31172ad6c3c62286191913b
ChangHo <tony9911@naver.com>
2023-11-27 01:36:50 +0900

    Feat: Image 엔티티 생성 Motivation: - 숙박 사진 정보를 저장하기 위한 이미지 인티티 생성
    Modification:
    - Image.java 파일 생성
    
    Signed-off-by: windowM <tony9911@naver.com>

---
+++ src/main/java/com/gamsung/backend/domain/image/Image.java
@@ -0,0 +1,50 @@
+package com.gamsung.backend.domain.image;
+
+import com.gamsung.backend.domain.accomodation.entity.Accomodation;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class Image {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    @Column(nullable = false)
+    Integer imgType;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "accomodation_id")
+    Accomodation accomodation;
+
+    @Column(nullable = false)
+    String url;
+
+    @Builder
+    private Image(
+        Accomodation accomodation,
+        Integer imgType,
+        String url
+    ) {
+        this.accomodation = accomodation;
+        this.imgType = imgType;
+        this.url = url;
+    }
+
+    public void setAccomodation(Accomodation accomodation) {
+        this.accomodation = accomodation;
+    }
+}
