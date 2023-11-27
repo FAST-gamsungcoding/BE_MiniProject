b33990e763955f0c9f1afde0843ec508267a3ace
rightpair <tkddn204@gmail.com>
2023-11-27 16:38:38 +0900

    Feat: `GlobalExceptionHandler`에 Validation 에러 추가
    Motivation:
    - `GlobalExceptionHandler`에 Validation 관련 에러를 추가하려 합니다.
    
    Modification:
    - `GlobalExceptionHandler`에 Validation 에러 추가
    
    Signed-off-by: rightpair <tkddn204@gmail.com>

--- src/main/java/com/gamsung/backend/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/gamsung/backend/global/exception/GlobalExceptionHandler.java
@@ -5,12 +5,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
     private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
-
 
     @ExceptionHandler(BaseException.class)
     public ResponseEntity<ApiResponse> handleBaseException(BaseException e) {
@@ -30,6 +32,17 @@ public class GlobalExceptionHandler {
                 ApiResponse.builder()
                         .code(Integer.parseInt(e.getCode()))
                         .data(e.getMessage())
+                        .build()
+        );
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<ApiResponse> handleBindValidationError(BindException e) {
+        logger.error(e.getMessage());
+        return ResponseEntity.badRequest().body(
+                ApiResponse.builder()
+                        .code(5003)
+                        .data("데이터 형식이 올바르지 않습니다.")
                         .build()
         );
     }
