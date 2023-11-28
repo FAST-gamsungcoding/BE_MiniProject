dca9ea668c6b504b0bda567dbaafe748d2f70eaa
rightpair <tkddn204@gmail.com>
2023-11-28 12:42:13 +0900

    Feat: 에러 메세지 추가
    Motivation:
    - #82 를 참고하세요.
    
    Modification:
    - 에러 메세지 추가
    
    Signed-off-by: rightpair <tkddn204@gmail.com>

--- src/main/java/com/gamsung/backend/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/gamsung/backend/global/exception/GlobalExceptionHandler.java
@@ -13,33 +13,32 @@ public class GlobalExceptionHandler {
 //    private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
     @ExceptionHandler(BaseException.class)
-    public ResponseEntity<ApiResponse<String>> handleBaseException(BaseException e) {
-
-        //실패 헤더에 담기는 코드는 부분은 badRequest가 아닌 경우 다르게 기본 http 상태코드를 확인 후 수정
+    public ResponseEntity<ApiResponse<ErrorMessage>> handleBaseException(BaseException e) {
         return ResponseEntity.badRequest().body(
-                ApiResponse.create(Integer.parseInt(e.getCode()), e.getMessage())
+                ApiResponse.create(Integer.parseInt(e.getCode()), ErrorMessage.create(e.getMessage()))
         );
     }
 
     @ExceptionHandler(UnAuthException.class)
-    public ResponseEntity<ApiResponse<String>> handleUnAuthException(UnAuthException e) {
+    public ResponseEntity<ApiResponse<ErrorMessage>> handleUnAuthException(UnAuthException e) {
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(
-                ApiResponse.create(Integer.parseInt(e.getCode()), e.getMessage())
+                ApiResponse.create(Integer.parseInt(e.getCode()), ErrorMessage.create(e.getMessage()))
         );
     }
 
     @ExceptionHandler(ForbiddenException.class)
-    public ResponseEntity<ApiResponse<String>> handleForbiddenException(ForbiddenException e) {
+    public ResponseEntity<ApiResponse<ErrorMessage>> handleForbiddenException(ForbiddenException e) {
         return ResponseEntity.status(HttpStatus.FORBIDDEN).body(
-                ApiResponse.create(Integer.parseInt(e.getCode()), e.getMessage())
+                ApiResponse.create(Integer.parseInt(e.getCode()), ErrorMessage.create(e.getMessage()))
         );
     }
 
     @ExceptionHandler(BindException.class)
-    public ResponseEntity<ApiResponse<String>> handleBindValidationError(BindException e) {
+    public ResponseEntity<ApiResponse<ErrorMessage>> handleBindValidationError(BindException e) {
         ErrorCode validationError = ErrorCode.VALIDATION_ERROR;
         return ResponseEntity.badRequest().body(
-                ApiResponse.create(Integer.parseInt(validationError.getCode()), validationError.getMessage())
+                ApiResponse.create(Integer.parseInt(validationError.getCode()),
+                        ErrorMessage.create(validationError.getMessage()))
         );
     }
 }
