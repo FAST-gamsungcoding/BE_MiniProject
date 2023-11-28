93e5fc43658c0b73c1dabf17d2aba8865928511f
pabu <socks1583@gmail.com>
2023-11-28 20:48:41 +0900

    Feat: 주문 API 개발
    Motivation:
     - Order 주문 API 개발
    
    Modification:
     - 예약 가능 조회 API
     - 구매한 내역(예약내역) 조회 API
     - 주문 결제(예약) API
     - 예외처리

--- src/main/java/com/gamsung/backend/domain/order/repository/OrderRepository.java
+++ src/main/java/com/gamsung/backend/domain/order/repository/OrderRepository.java
@@ -13,13 +13,12 @@ import java.util.List;
 public interface OrderRepository extends JpaRepository<Order, Long> {
     List<Order> findByMemberIdOrderByCreatedAtDesc(long memberId, Pageable pageable);
 
-    @Query("SELECT IF(COUNT(o) > 0, true, false) " +
-            "FROM Orders o " +
-            "WHERE o.accommodationId = :accommodationId AND o.startDate < :endDate AND o.endDate > :startDate")
+//    @Query("SELECT * FROM orders o WHERE o.accommodationId = ?1 AND o.startDate < ?2 AND o.endDate > ?3")
     boolean existsByAccommodationIdAndStartDateBeforeAndEndDateAfter(
             @Param("accommodationId") long accommodationId,
             @Param("endDate") LocalDate endDate,
             @Param("startDate") LocalDate startDate);
+
 
     // 주문 테이블에서 1. 해당 숙소의 id값을 숙소 외래키로 가지고
     // 2. 예약 시작일이 사용자의 예약 종료일보다 작고
