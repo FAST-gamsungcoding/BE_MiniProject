c09513f195262801ed082098687a087a83f77e9f
parkgeonwoo <50697545+Parkgeonmoo@users.noreply.github.com>
2023-11-29 17:59:08 +0900

    Refactor : Swagger 불필요한 부분 삭제 및 수정

--- src/main/java/com/gamsung/backend/domain/cart/controller/CartController.java
+++ src/main/java/com/gamsung/backend/domain/cart/controller/CartController.java
@@ -7,21 +7,16 @@ import com.gamsung.backend.domain.cart.dto.response.CartEntryResponse;
 import com.gamsung.backend.domain.cart.dto.response.CartFindResponse;
 import com.gamsung.backend.domain.cart.service.CartService;
 import com.gamsung.backend.global.common.ApiResponse;
-import com.gamsung.backend.global.config.UserDetailsConfig;
 import com.gamsung.backend.global.resolver.AuthContext;
 import com.gamsung.backend.global.resolver.MemberAuth;
 import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
-import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-import static com.gamsung.backend.global.config.SwaggerDescriptionConfig.*;
 
 @RestController
 @RequestMapping("/v1/cart")
@@ -31,7 +26,7 @@ public class CartController {
     private final CartService cartService;
 
     @PostMapping
-    @Operation(summary = "장바구니에 상품 추가 API", description = ENTRY_MY_CART)
+    @Operation(summary = "장바구니에 상품 추가 API", description = "로그인한 회원의 장바구니에 상품을 추가할 때 쓰는 API")
     public ResponseEntity<ApiResponse<CartEntryResponse>> entryMyCart(@Valid @RequestBody CartEntryRequest cartEntryRequest
     , @MemberAuth AuthContext authContext) {
         Long memberId = authContext.id();
@@ -42,7 +37,7 @@ public class CartController {
 
 
     @GetMapping
-    @Operation(summary = "장바구니 목록 확인 API", description = FIND_MY_CART)
+    @Operation(summary = "장바구니 목록 확인 API", description = "로그인한 회원의 장바구니 목록을 확인하고 품절여부를 나타내는 API")
     public ResponseEntity<ApiResponse<List<CartFindResponse>>> findMyCart(@MemberAuth AuthContext authContext) {
         Long memberId = authContext.id();
         List<CartFindResponse> myCartList = cartService.findMyCart(memberId);
@@ -50,7 +45,7 @@ public class CartController {
     }
 
     @DeleteMapping
-    @Operation(summary = "장바구니 삭제 API", description = DELETE_MY_CART)
+    @Operation(summary = "장바구니 삭제 API", description = "로그인한 회원의 장바구니 목록에서 삭제할 장바구니를 선택 시 삭제시키는 API")
     public ResponseEntity<ApiResponse<CartDeleteResponse>> deleteMyCart(@Valid @RequestBody CartDeleteRequest cartDeleteRequest,
                                                                         @MemberAuth AuthContext authContext) {
         Long memberId = authContext.id();
