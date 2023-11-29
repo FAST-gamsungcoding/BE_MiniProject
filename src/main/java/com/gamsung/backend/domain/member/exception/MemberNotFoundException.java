1f76d44db169e94e19657eb0f215b280e6a4a45d
rightpair <tkddn204@gmail.com>
2023-11-29 16:44:47 +0900

    Fix: Exception을 받지 못하는 오류 수정
    Motivation:
    - JwtExceptionFilter에서 제대로 오류를 catch하지 못하는 오류를 수정하려 합니다.
    
    Modification:
    - `JwtExceptionFilter.java` 수정
    - 기타 jwt 관련 Exception 클래스 수정
    
    Signed-off-by: rightpair <tkddn204@gmail.com>

--- src/main/java/com/gamsung/backend/domain/member/exception/MemberNotFoundException.java
+++ src/main/java/com/gamsung/backend/domain/member/exception/MemberNotFoundException.java
@@ -1,9 +1,9 @@
 package com.gamsung.backend.domain.member.exception;
 
+import com.gamsung.backend.global.exception.BaseException;
 import com.gamsung.backend.global.exception.ErrorCode;
-import com.gamsung.backend.global.exception.UnAuthException;
 
-public class MemberNotFoundException extends UnAuthException {
+public class MemberNotFoundException extends BaseException {
     public MemberNotFoundException() {
         super(ErrorCode.MEMBER_NOT_FOUND);
     }
--- src/main/java/com/gamsung/backend/global/jwt/exception/JwtInvalidAccessTokenException.java
+++ src/main/java/com/gamsung/backend/global/jwt/exception/JwtInvalidAccessTokenException.java
@@ -1,9 +1,9 @@
 package com.gamsung.backend.global.jwt.exception;
 
 import com.gamsung.backend.global.exception.ErrorCode;
-import com.gamsung.backend.global.exception.ForbiddenException;
+import com.gamsung.backend.global.exception.UnAuthException;
 
-public class JwtInvalidAccessTokenException extends ForbiddenException {
+public class JwtInvalidAccessTokenException extends UnAuthException {
     public JwtInvalidAccessTokenException() {
         super(ErrorCode.JWT_INVALID_ACCESS_TOKEN);
     }
